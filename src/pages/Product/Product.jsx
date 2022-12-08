0e917082c4fd02ab9d7cc35daa47c5b133b07926
fCaioRibeiro <kkaaiioo70@gmail.com>
2022-12-08 09:42:52 -0300

    implementando compartilhar produto

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,29 +1,43 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { useRef } from "react";
+import { Alert, Button, Share, StyleSheet, Text, View } from "react-native";
 import QRCode from "react-native-qrcode-svg";
+import ViewShot from "react-native-view-shot";
 
 export default function ProductPage({ route, navigation }) {
   const { product } = route.params;
 
-  return (
-    <View
-      style={styles.card}
-    >
-      <Text style={{ fontSize: 25, fontWeight: 'bold', marginBottom: 20 }}>{product.name}</Text>
-      <Text style={{ fontSize: 20, marginBottom: 3 }}>Valor: {product.price}</Text>
-      <Text style={{ fontSize: 20, marginBottom: 3 }}>Quantidade: {product.amount}</Text>
-      <Text style={{ fontSize: 20, marginBottom: 3 }}>Undiade de medida: {product.measurement}</Text>
-      <View style={{ marginVertical: 30, display: 'flex', alignItems: 'center' }}>
-        <QRCode
-          size={200}
-          value={product.id}
-        />
-      </View>
+  const ref = useRef();
 
-      {/* <Button
-        title="Imprimir"
+  const onCapture = () => {
+    ref.current.capture().then(uri => {
+      Share.share({
+        title: product.name,
+        url: uri
+      })
+    });
+  };
+
+  return (
+    <>
+      <ViewShot ref={ref} style={styles.card} options={{ fileName: "Your-File-Name", format: "jpg", quality: 0.9 }}>
+        <Text style={{ fontSize: 25, fontWeight: 'bold', marginBottom: 20 }}>{product.name}</Text>
+        <Text style={{ fontSize: 20, marginBottom: 3 }}>Valor: {product.price}</Text>
+        <Text style={{ fontSize: 20, marginBottom: 3 }}>Quantidade: {product.amount}</Text>
+        <Text style={{ fontSize: 20, marginBottom: 3 }}>Undiade de medida: {product.measurement}</Text>
+        <View style={{ marginVertical: 30, display: 'flex', alignItems: 'center' }}>
+          <QRCode
+            size={200}
+            value={product.id}
+          />
+        </View>
+      </ViewShot>
+
+      <Button
+        title="Compartilhar"
         color="#000"
-      /> */}
-    </View>
+        onPress={onCapture}
+      />
+    </>
   );
 }
 
